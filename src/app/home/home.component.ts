9533f13d69eeb8e25b3cc8c7c00374ff6d81830f
prescott breeden <prescott.breeden@gmail.com>
2018-06-28 03:27:35 -0700

    gradient to tech background
    cleaned up the preload code a bit as well

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  total_images = 35;
+  total_images = 28;
   images = [];
 
   constructor() { }
