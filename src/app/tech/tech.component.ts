1d2295c326bdd9bb62df207a69dd459ec9603920
prescott breeden <prescott.breeden@gmail.com>
2018-06-27 23:36:38 -0700

    randomized animation finished

--- src/app/tech/tech.component.ts
+++ src/app/tech/tech.component.ts
@@ -12,8 +12,19 @@ images = [];
   constructor() { }
 
   ngOnInit() {
+    this.randomizeHexes();
+  }
+
+  randomizeHexes() {
     for (let i = 1; i <= this.total_images; i++) {
       this.images.push(i);
+    }
+
+    for (let i = 0; i < this.images.length; i++) {
+      const swap = Math.floor(Math.random() * (i + 1));
+      const temp = this.images[i];
+      this.images[i] = this.images[swap];
+      this.images[swap] = temp;
     }
   }
 
